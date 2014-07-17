7139aa0eb3c02f707d7af4e2edc474b60c70ff59
Richard Attermeyer <richard.attermeyr@gmail.com>
2014-07-17 19:39:35 +0000

    syntax corrected

--- manifests/developers.pp
+++ manifests/developers.pp
@@ -3,7 +3,6 @@ class user::developers {
 	file  { [ '/home/dev/.config', '/home/dev/.config/dconf' ] :
 		ensure => directory,
 		owner => 'dev',
-		shell => '/bin/bash',
 		recurse => true
 	}
 }
--- manifests/virtual.pp
+++ manifests/virtual.pp
@@ -2,6 +2,7 @@ class user::virtual {
 	@user { 'dev': 
 		ensure => present,
 		managehome => true,
-		password => '$6$NHCpS7VJuQ/7Gh$tBZgdn1ZF8Sub91bgXk7IKJ4gFe2BXmbxrTn9BCuI761Uawa1kcGW1VRAl7KTvjeqYkoo06kmE1ZIkgbWGGLr1'
+		password => '$6$NHCpS7VJuQ/7Gh$tBZgdn1ZF8Sub91bgXk7IKJ4gFe2BXmbxrTn9BCuI761Uawa1kcGW1VRAl7KTvjeqYkoo06kmE1ZIkgbWGGLr1',
+		shell    => '/bin/bash'
 	}
 }
